5ea3755d75aa9c7f199e51ff32c5e02d13639f5c
vinayak.k <104497325+vinayakkunder97@users.noreply.github.com>
2023-12-12 23:35:03 +0530

    Update server.ts

--- express/server.ts
+++ express/server.ts
@@ -13,6 +13,6 @@ app.use(
 );
 
 const PORT = process.env.PORT || 4000;
-app.listen({ host: '0.0.0.0', port: ${PORT} }, () => {
+app.listen({ host: '0.0.0.0', port: 4000 }, () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}/graphql`);
 });
